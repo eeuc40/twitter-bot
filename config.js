d780ed433e54343f05ece2d86e1d2b476f01b56c
Richie Hughes <me@richiehughes.co.uk>
2015-12-03 20:21:51 +0000

    Removed the old keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'Fzqy4zqIJ48NowdwgfatcjMp',
-    consumer_secret: 'rMuJoJTCKHKijxs8Bbp04d6KxnmIDMS7Q285hidIWTm04u7Oc',
-    access_token: '598515634-gDi4C65khTuUsGCawYHnJyAAlti4sQanSKjKuFH',
-    access_token_secret: 'e8tUVoPgOomvTWEEBYrxsiQAUJQeCw9xh5O9TOZlL5bN'
+    consumer_key: 'CONSUMER_KEY',
+    consumer_secret: 'CONSUMER_SECRET',
+    access_token: 'ACCESS_TOKEN',
+    access_token_secret: 'ACCESS_TOKEN_SECRET'
 };
