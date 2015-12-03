8953dcdde8c2c37bea2bc16a03282071db9df6b8
Richie Hughes <me@richiehughes.co.uk>
2015-12-03 20:03:45 +0000

    Added Keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+    consumer_key: 'Fzqy4zqIJ48NowdwgfatcjMp',
+    consumer_secret: 'rMuJoJTCKHKijxs8Bbp04d6KxnmIDMS7Q285hidIWTm04u7Oc',
+    access_token: '598515634-gDi4C65khTuUsGCawYHnJyAAlti4sQanSKjKuFH',
+    access_token_secret: 'e8tUVoPgOomvTWEEBYrxsiQAUJQeCw9xh5O9TOZlL5bN'
 };
